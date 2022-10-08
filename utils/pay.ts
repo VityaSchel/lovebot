9ede4bfe8c697c7389a5972c4d4c3e382e3ee23b
Alexander Zhuravlev <5747861-MixailRadionov@users.noreply.gitlab.com>
2022-10-09 00:19:28 +0300

    fixed

--- utils/pay.ts
+++ utils/pay.ts
@@ -6,31 +6,36 @@ type payProps = {
 }
 
 export function pay(props: payProps) {
-  try {
-    const widget = new cp.CloudPayments()
+    try {
 
-    const { cloudpayments, onSuccess } = props
+        console.log('click pay');
 
-    widget.pay(
-      'charge', // auth или charge
-      cloudpayments,
-      {
-        onSuccess: function(options: any) {
-          // действие при успешной оплате
-         // onSuccess()
-        },
-        onFail: function(reason: any, options: any) {
-          // действие при неуспешной оплате
-          console.log('cloudpayments fail: ', reason)
-        },
-        onComplete: function(paymentResult: any, options: any) {
-          //  Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
-          // Например, вызов вашей аналитики
-            Cookies.delete('visited')
-        }
-      }
-    )
-  } catch (error: any) {
-    console.log('cloudpayments error: ', error)
-  }
+        const widget = new cp.CloudPayments()
+
+        const { cloudpayments, onSuccess } = props
+
+        widget.pay(
+            'charge', // auth или charge
+            cloudpayments,
+            {
+                onSuccess: function(options: any) {
+                    // действие при успешной оплате
+                    // onSuccess()
+                    console.log('cloudpayments success')
+                },
+                onFail: function(reason: any, options: any) {
+                    // действие при неуспешной оплате
+                    console.log('cloudpayments fail: ', reason)
+                },
+                onComplete: function(paymentResult: any, options: any) {
+                    //  Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
+                    // Например, вызов вашей аналитики
+                    console.log('cloudpayments complete: ', paymentResult)
+                    Cookies.delete('visited')
+                }
+            }
+        )
+    } catch (error: any) {
+        console.log('cloudpayments error: ', error)
+    }
 }
